b35463fa36e59e7dfefcd218ca022b2f21c8f121
angelo <angelo.marney@gmail.com>
2024-02-22 00:24:34 -0600

    rf: janky fish avoidance

--- fish/FishMultimesh.cs
+++ fish/FishMultimesh.cs
@@ -5,11 +5,12 @@ namespace DolphinForces;
 
 public partial class FishMultimesh : MultiMeshInstance3D {
 
-    const int NUM_FISH = 2000;
-    const float MIN_DEPTH = 0f;
+    [Export] Node3D _avoid;
+
+    const int NUM_FISH = 2500;
+    const float MIN_DEPTH = -3f;
     const float MAX_DEPTH = -100f;
-    const float WANDER_DEPTH = 30f;
-    const float WANDER_XY_DIST = 30f;
+    const float WANDER_DIST = 30f;
 
     static float RandInRange(float min, float max) =>
          (float)Main.Rng.NextDouble() * (max - min) + min;
@@ -17,22 +18,24 @@ public partial class FishMultimesh : MultiMeshInstance3D {
     Vector3[] _targets = new Vector3[NUM_FISH];
 
     public override void _Ready() {
+        Debug.Assert(_avoid is Player);
         Multimesh.InstanceCount = NUM_FISH;
 
         for (var fishId = 0; fishId < NUM_FISH; fishId++) {
+            _targets[fishId] = RandPos(_targets[fishId], 1f, WANDER_DIST);
+            _targets[fishId] = _targets[fishId] with {
+                Y = Mathf.Clamp(_targets[fishId].Y, MAX_DEPTH, MIN_DEPTH)
+            };
+
             const float initRange = 25f;
             var randOffset = new Vector3(
                 x: RandInRange(-initRange, initRange),
                 y: RandInRange(MAX_DEPTH, MIN_DEPTH),
                 z: RandInRange(-initRange, initRange)
             );
-
-            _targets[fishId] = RandPos(_targets[fishId], 1f, WANDER_XY_DIST, WANDER_DEPTH);
-            _targets[fishId] = _targets[fishId] with {
-                Y = Mathf.Clamp(_targets[fishId].Y, MAX_DEPTH, MIN_DEPTH)
-            };
-
-            Multimesh.SetInstanceTransform(fishId, Transform3D.Identity.Translated(randOffset));
+            var randTransform = Transform3D.Identity.Translated(randOffset);
+            randTransform = randTransform.LookingAt(_targets[fishId]);
+            Multimesh.SetInstanceTransform(fishId, randTransform);
 
             Multimesh.SetInstanceCustomData(fishId,
                 new Color(
@@ -55,7 +58,7 @@ public partial class FishMultimesh : MultiMeshInstance3D {
             var isFishAtDestination = fishTransform.Origin.DistanceTo(_targets[fishId]) < 1f;
             var isChangeDirRandomly = (float)Main.Rng.NextDouble() > .01f;
             if (isFishAtDestination || isChangeDirRandomly) {
-                _targets[fishId] = RandPos(_targets[fishId], 1f, WANDER_XY_DIST, WANDER_DEPTH);
+                _targets[fishId] = RandPos(_targets[fishId], 1f, WANDER_DIST);
                 _targets[fishId] = _targets[fishId] with {
                     Y = Mathf.Clamp(_targets[fishId].Y, MAX_DEPTH, MIN_DEPTH)
                 };
@@ -65,18 +68,25 @@ public partial class FishMultimesh : MultiMeshInstance3D {
 
             var targetDir = fishTransform.Origin.DirectionTo(_targets[fishId]);
             const float moveAmount = 0.2f;
+            var avoidanceScaleFactor =
+                fishTransform.Origin.DistanceTo(_avoid.GlobalPosition) < 5f ?
+                5f : 1f;
             fishTransform = fishTransform.TranslatedLocal(
-                (float)Main.Rng.NextDouble() * moveAmount * targetDir);
+                (float)Main.Rng.NextDouble() * moveAmount * avoidanceScaleFactor * targetDir);
             Multimesh.SetInstanceTransform(fishId, fishTransform);
         }
     }
 
-    Vector3 RandPos(Vector3 startingPos, float minDist, float maxDist, float heightRange) {
-        var randDist = RandInRange(minDist, maxDist);
-        var randHeight = RandInRange(0f, heightRange);
-        var randAngle = (float)Main.Rng.NextDouble() * 2 * Mathf.Pi;
-        var randDir = new Vector3(Mathf.Cos(randAngle), 0, Mathf.Sin(randAngle)).Normalized();
-        return startingPos + (randHeight * Vector3.Up) + (randDir * randDist);
+    Vector3 RandPos(Vector3 startingPos, float minDist, float maxDist) {
+        float randDist = RandInRange(minDist, maxDist);
+        float thetaAzimuthalAngle = (float)Main.Rng.NextDouble() * 2 * Mathf.Pi;
+        float phiPolarAngle = Mathf.Acos(2 * (float)Main.Rng.NextDouble() - 1);
+        Vector3 randDir = new Vector3(
+            Mathf.Sin(phiPolarAngle) * Mathf.Cos(thetaAzimuthalAngle),
+            Mathf.Sin(phiPolarAngle) * Mathf.Sin(thetaAzimuthalAngle),
+            Mathf.Cos(phiPolarAngle)
+        );
+        return startingPos + randDir * randDist;
     }
 
 }
