a62275a66b2553101b1b81b4f724232cee04d945
angelo <angelo.marney@gmail.com>
2024-02-21 18:34:27 -0600

    rf: delete unused extension

--- GodotExtensions.cs
+++ GodotExtensions.cs
@@ -6,21 +6,6 @@ namespace DolphinForces;
 /// <summary> Static helper extension methods for Godot types. </summary>
 public static class GodotExtensions {
 
-    /// <summary> Returns the first descendant of a given type using a 
-    /// breadth-first search. </summary>
-    public static T? GetDescendant<T>(this Node @this) {
-        var queue = new Queue<Node>();
-        queue.Enqueue(@this);
-        while (queue.Count > 0) {
-            var current = queue.Dequeue();
-            if (current is T match)
-                return match;
-            foreach (var child in current.GetChildren())
-                queue.Enqueue(child);
-        }
-        return default;
-    }
-
     /// <summary> Returns list of descendants of a given type, ordered using
     /// a breadth-first search. </summary>
     public static List<T> Descendants<T>(this Node @this) {
