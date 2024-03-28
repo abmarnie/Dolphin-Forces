be078194c8ace6504b172362c2fccc7b3affc258
angelo <angelo.marney@gmail.com>
2024-03-28 01:34:42 -0500

    rf: minor clean

--- fish_multimesh/FishMultimesh.cs
+++ fish_multimesh/FishMultimesh.cs
@@ -7,7 +7,8 @@ namespace DolphinForces;
 public partial class FishMultimesh : MultiMeshInstance3D {
 
     // TODO: Stricter type (only avoid player).
-    [Export] Node3D _playerToAvoid = null!;
+    // TODO: Should player have a static instance?
+    [Export] Node3D _player = null!;
 
     // The fish shouldn't go too deep or too shallow. They are supposed to
     // sort of be used by the player to gauge depth. I probably should also
@@ -20,34 +21,38 @@ public partial class FishMultimesh : MultiMeshInstance3D {
     static float RandInRange(float min, float max) =>
          (float)Main.Rng.NextDouble() * (max - min) + min;
 
-    Vector3[] _wanderTargets = new Vector3[NUM_FISH];
+    Vector3[] _wanderPos = new Vector3[NUM_FISH];
 
     public override void _Ready() {
 
-        Debug.Assert(_playerToAvoid is Player);
+        Debug.Assert(_player is Player);
         Debug.Assert(Multimesh.TransformFormat == MultiMesh.TransformFormatEnum.Transform3D);
         Debug.Assert(Multimesh.UseCustomData);
+        Debug.Assert((Multimesh.Mesh as ArrayMesh)!.CustomAabb.Size.X > 1000f);
+        Debug.Assert((Multimesh.Mesh as ArrayMesh)!.CustomAabb.Size.Y > 1000f);
+        Debug.Assert((Multimesh.Mesh as ArrayMesh)!.CustomAabb.Size.Z > 1000f);
 
         Multimesh.InstanceCount = NUM_FISH;
 
         for (var fishId = 0; fishId < NUM_FISH; fishId++) {
 
-            // Randomize the fish wander target.
-            _wanderTargets[fishId] = RandomWanderTargetPosition(startPos: Vector3.Zero) with {
-                Y = Mathf.Clamp(_wanderTargets[fishId].Y, MAX_DEPTH, MIN_DEPTH)
+            // Set initial fish wander target.
+            _wanderPos[fishId] = RandomWanderTargetPos(startPos: Vector3.Zero);
+            _wanderPos[fishId] = _wanderPos[fishId] with {
+                Y = Mathf.Clamp(_wanderPos[fishId].Y, MAX_DEPTH, MIN_DEPTH)
             };
 
-            // Randomize the fish spawn transform.
+            // Set fish spawn location.
             const float initHorzRange = 25f;
             var randTransform = Transform3D.Identity.Translated(new Vector3(
                 x: RandInRange(-initHorzRange, initHorzRange),
                 y: RandInRange(MAX_DEPTH, MIN_DEPTH),
                 z: RandInRange(-initHorzRange, initHorzRange)
             ));
-            randTransform = randTransform.LookingAt(_wanderTargets[fishId]);
+            randTransform = randTransform.LookingAt(_wanderPos[fishId]);
             Multimesh.SetInstanceTransform(fishId, randTransform);
 
-            // Randomize the fish
+            // Prevent fish animation shaders from being perfectly synchronized.
             Multimesh.SetInstanceCustomData(fishId,
                 new Color(
                     RandInRange(0, 1),
@@ -56,42 +61,50 @@ public partial class FishMultimesh : MultiMeshInstance3D {
                     RandInRange(0, 1)
                 )
             );
+
         }
+
     }
 
     public override void _PhysicsProcess(double delta) {
+
         if (!Main.IsGameStarted()) {
             return;
         }
 
-        for (var fishId = 0; fishId < NUM_FISH; fishId++) {
-            var fishTransform = Multimesh.GetInstanceTransform(fishId);
-            var isFishAtDestination = fishTransform.Origin.DistanceTo(_wanderTargets[fishId]) < 1f;
-            var isChangeDirRandomly = (float)Main.Rng.NextDouble() > .01f;
-            if (isFishAtDestination || isChangeDirRandomly) {
-                _wanderTargets[fishId] = RandomWanderTargetPosition(_wanderTargets[fishId]);
-                _wanderTargets[fishId] = _wanderTargets[fishId] with {
-                    Y = Mathf.Clamp(_wanderTargets[fishId].Y, MAX_DEPTH, MIN_DEPTH)
-                };
+        for (var id = 0; id < NUM_FISH; id++) {
 
-                fishTransform = fishTransform.LookingAt(_wanderTargets[fishId]);
+            // Set the wander target randomly and look at the target. 
+            var transform = Multimesh.GetInstanceTransform(id);
+            var isAtDest = transform.Origin.DistanceTo(_wanderPos[id]) < 1f;
+            var isChangeDirRandomly = (float)Main.Rng.NextDouble() > .01f;
+            if (isAtDest || isChangeDirRandomly) {
+                _wanderPos[id] = RandomWanderTargetPos(_wanderPos[id]);
+                _wanderPos[id] = _wanderPos[id] with {
+                    Y = Mathf.Clamp(_wanderPos[id].Y, MAX_DEPTH, MIN_DEPTH)
+                };
+                transform = transform.LookingAt(_wanderPos[id]);
             }
 
-            var targetDir = fishTransform.Origin.DirectionTo(_wanderTargets[fishId]);
+            // Move the fish a small amount. The speed of movement depends on 
+            // if the player is nearby.
+            var wanderDir = transform.Origin.DirectionTo(_wanderPos[id]);
             const float moveAmount = 0.2f;
             var avoidanceScaleFactor =
-                fishTransform.Origin.DistanceTo(_playerToAvoid.GlobalPosition) < 5f ?
-                5f : 1f;
-            fishTransform = fishTransform.TranslatedLocal(
-                (float)Main.Rng.NextDouble() * moveAmount * avoidanceScaleFactor * targetDir);
-            Multimesh.SetInstanceTransform(fishId, fishTransform);
+                transform.Origin.DistanceTo(_player.GlobalPosition) < 5f ? 5f
+                : 1f;
+            transform = transform.TranslatedLocal(
+                (float)Main.Rng.NextDouble() * moveAmount * avoidanceScaleFactor * wanderDir);
+            Multimesh.SetInstanceTransform(id, transform);
+
         }
+
     }
 
-    static Vector3 RandomWanderTargetPosition(Vector3 startPos) {
-        const float wanderDistMin = 1f;
-        const float wanderDistMax = 30f;
-        var randDist = RandInRange(wanderDistMin, wanderDistMax);
+    static Vector3 RandomWanderTargetPos(Vector3 startPos) {
+        const float minDist = 1f;
+        const float maxDist = 30f;
+        var randDist = RandInRange(minDist, maxDist);
         var azimuthAngle = (float)Main.Rng.NextDouble() * 2 * Mathf.Pi;
         var polarAngle = Mathf.Acos(2 * (float)Main.Rng.NextDouble() - 1);
         var randDir = new Vector3(
@@ -99,7 +112,7 @@ public partial class FishMultimesh : MultiMeshInstance3D {
             Mathf.Sin(polarAngle) * Mathf.Sin(azimuthAngle),
             Mathf.Cos(polarAngle)
         );
-        return startPos + randDir * randDist;
+        return startPos + randDist * randDir;
     }
 
 }
